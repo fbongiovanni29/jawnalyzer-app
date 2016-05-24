650562029a8e84d74aed04a493480af45286f3e2
Francis Bongiovanni <francisjbongiovanni@github.com>
2016-05-24 14:45:50 -0400

    small refactor

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-var express = require('express');
-var app = express();
+  var express = require('express');
+  var app = express();
 
 
-// Tell app.js about the home controller using require
-var homeController = require('./controllers/home');
-var apiController = require('./controllers/api')
+  // Tell app.js about the home controller using require
+  var homeController = require('./controllers/home');
+  var apiController = require('./controllers/api')
 
-// Serve static files (i.e. images, scripts, styles, templates) from public/ directory
-app.use(express.static('public'));
+  //Jade
+  app.set('view engine', 'pug');
 
-//Jade
-app.set('view engine', 'pug');
+  // Routes
+  app.get('/', homeController.index);
 
-// Routes
-app.get('/', homeController.index);
-
-
-app.get('/api/all', apiController.all);
-app.get('/api/get/:id', apiController.get);
+  app.get('/api/all', apiController.all);
+  app.get('/api/get/:id', apiController.get);
 
 
-app.get('/*', homeController.noRoute)
+  app.get('/*', homeController.noRoute)
 
-app.listen(4567, function () {
+  app.listen(4567, function () {
     console.log('Your app is listening on port 4567!');
-});
+  });
--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,7 @@
 var request = require('request');
 
-// exports.index = function (req, res) {
-//   res.render('index');
-// }
-
 exports.index = function (req, res) {
   request('http://localhost:4567/api/get/0', function(err, response, body) {
-      console.log(Object.keys(JSON.parse(body).technologies))
     if(!err && response.statusCode == 200) {
         res.render('index', {
             technologies: Object.keys(JSON.parse(body).technologies)
