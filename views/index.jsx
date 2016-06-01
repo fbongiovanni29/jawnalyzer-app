9ebeb3e2ae150ae4ed206f75074be91910bdca77
Francis Bongiovanni <francisjbongiovanni@github.com>
2016-05-31 21:12:15 -0400

    data displays

--- views/index.jsx
+++ views/index.jsx
@@ -7,24 +7,39 @@ var Jawn = React.createClass({
   render: function() {
         return (
           <div>
-              <h1>REMOTE: {this.state.remote}</h1>
-              {this.state.title.map(function(title){
-                    return <h1>{title.type} {title.amount}</h1>;
-              })}
-              {this.state.technologies.map(function(technology){
-                    return <h1>{technology.type} {technology.amount}</h1>;
-              })}
-              {this.state.employmentType.map(function(empType){
-                    return <h1>{empType.type} {empType.amount}</h1>;
-              })}
-              <h1>COMPANIES: {this.state.companies}</h1>
-              {this.state.jobLocation.map(function(jobLoc){
-                    return <h1>{jobLoc.city} {jobLoc.amount}</h1>;
-              })}
-              {this.state.type.map(function(type){
-                    return <h1>{type.type} {type.amount}</h1>;
-              })}
-         </div>
+            <div>
+                <h1>Amount of Companies: {this.state.companies}</h1>
+                <h1>Amount Remote: {this.state.remote}</h1>
+          </div>
+          <div>
+                  {this.state.title.map(function(title){
+                        return <h1>{title.type}: {title.amount}</h1>;
+                  })}
+          </div>
+            <div>
+                {this.state.technologies.map(function(technology){
+                      return <h1>{technology.type} {technology.amount}</h1>;
+                })}
+          </div>
+            <div>
+                {this.state.employmentType.map(function(empType){
+                      return <h1>{empType.type} {empType.amount}</h1>;
+                })}
+          </div>
+            <div>
+                <h1>COMPANIES: {this.state.companies}</h1>
+          </div>
+            <div>
+                {this.state.jobLocation.map(function(jobLoc){
+                      return <h1>{jobLoc.city} {jobLoc.amount}</h1>;
+                })}
+          </div>
+            <div>
+                {this.state.type.map(function(type){
+                      return <h1>{type.type} {type.amount}</h1>;
+                })}
+          </div>
+          </div>
         );
     }
   });
